a0eeea762e81fe316ac7c5f391ee4f8f150c57e4
Sandeep Verma <sandeepskvverma1@outlook.com>
2023-10-05 12:19:41 +0530

    Update News_showing_project.py

--- News_showing_project.py
+++ News_showing_project.py
@@ -2,9 +2,10 @@ import requests
 import json
 
 query = input("Which type of news you want to see? ")
-date = input("Enter the news date (YYYY-MM-DD): ")
+from_date = input("Enter the news date (YYYY-MM-DD): ")
+to_date = input("Enter the news date (YYYY-MM-DD): ")
 
-url = f"https://newsapi.org/v2/everything?q={query}&from={date}&sortBy=publishedAt&apiKey=ad1337ae1a9840629ce5a231aadcd120"
+url = f"https://newsapi.org/v2/everything?q={query}&from={from_date}&to={to_date}&sortBy=publishedAt&apiKey=ad1337ae1a9840629ce5a231aadcd120"
 
 r = requests.get(url)
 news = json.loads(r.text)
